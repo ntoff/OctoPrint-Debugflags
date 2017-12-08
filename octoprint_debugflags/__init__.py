5e67c9685714540312b2c9d556b59bd0fb47a37b
ntoff <norahtihtzoff@gmail.com>
2017-12-08 20:39:12 +1000

    remove stuff from init py that isn't used

--- octoprint_debugflags/__init__.py
+++ octoprint_debugflags/__init__.py
@@ -1,44 +1,19 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
-class DebugflagsPlugin(octoprint.plugin.SettingsPlugin,
-                       octoprint.plugin.AssetPlugin,
+class DebugflagsPlugin(octoprint.plugin.AssetPlugin,
                        octoprint.plugin.TemplatePlugin):
 
-	##~~ SettingsPlugin mixin
-
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
-
-	##~~ AssetPlugin mixin
-
 	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
 		return dict(
 			js=["js/debugflags.js"],
 			css=["css/debugflags.css"],
 			less=["less/debugflags.less"]
 		)
 
-	##~~ Softwareupdate hook
-
 	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
 		return dict(
 			debugflags=dict(
 				displayName="Debugflags Plugin",
@@ -55,11 +30,8 @@ class DebugflagsPlugin(octoprint.plugin.SettingsPlugin,
 			)
 		)
 
-
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Debugflags Plugin"
+__plugin_name__ = "Repetier Debug Flags"
+__plugin_description__ = "Buttons to set Repetier Firmware's debug flags (some may or may not work depending on firmware version)."
 
 def __plugin_load__():
 	global __plugin_implementation__
